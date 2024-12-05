4b8ec48abbec276471f673a9685545efc036175f
GustavoOB <112260397+Gustavo-Oliveira-Brandao@users.noreply.github.com>
2024-12-05 18:51:43 -0300

    feat: Models Alerta e Usuario criados

---
+++ MonitoramentoAmbiental/Controllers/AlertaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonitoramentoAmbiental.Models;
+
+namespace MonitoramentoAmbiental.Controllers
+{
+    public class AlertaController : Controller
+    {
+
+        [HttpPost]
+        public async Alerta Criar()
+        {
+            return View();
+        }
+    }
+}
